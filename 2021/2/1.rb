457e2fc85a66c8fe0857db986f23560421794043
kaelonswanton <kaelonswanton@gmail.com>
2024-01-18 14:34:18 -0800

    .

--- 2021/2/1.rb
+++ 2021/2/1.rb
@@ -1,47 +1,49 @@
-class FileType
-  def self.to_array(file)
-    File.read(file).split("\n")
-  end
-end
-
 class Movement
-  attr_reader :array
-
   def self.from_file(path)
-    array = FileType.to_array(path)
-    new(array)
+    new(File.readlines(path))
   end
 
-  def initialize(array)
-    @array = array
+  def initialize(lines)
+    @lines = lines
+  end
+
+  def call
+    depth * horizontal
+  end
+
+  private
+
+  def depth
+    down - up
   end
 
   def forward
-    forward_movements = array.select { |movement| movement.include?('forward') }
-    forward_movements.map { |movement| movement.split(' ').last.to_i }.sum
-  end
-
-  def horizontal
-    forward
+    movement_for('forward')
   end
 
   def up
-    up_movements = array.select { |movement| movement.include?('up') }
-    up_movements.map { |movement| movement.split(' ').last.to_i }.sum
+    movement_for('up')
   end
 
   def down
-    down_movements = array.select { |movement| movement.include?('down') }
-    down_movements.map { |movement| movement.split(' ').last.to_i }.sum
+    movement_for('down')
   end
 
   def depth
     down - up
   end
 
-  def result
-    depth * horizontal
+  def horizontal
+    forward
   end
+
+  def movement_for(direction)
+    @lines
+      .select { |line| line.include?(direction) }
+      .map { |movement| movement.split(' ').last.to_i}
+      .sum
+  end
+
 end
- 
-p Movement.from_file('file').result
+
+p Movement.from_file('file').call
---
+++ 2023/1/1.rb
@@ -0,0 +1,40 @@
+class Puzzle
+  attr_reader :lines
+  def self.from_file(path)
+    new(File.readlines(path).map(&:chomp))
+  end
+
+  def initialize(lines)
+    @lines = lines
+  end
+
+  def call
+    join_digits.sum
+  end
+
+  private
+
+  def parse_digits
+    lines.map do |line|
+      line.scan(/\d/)
+    end
+  end
+
+  def double_digits
+    parse_digits.map do |subarray|
+      if subarray.length == 1
+        [subarray.first] + [subarray.first]
+      else
+        subarray
+      end
+    end
+  end
+
+  def join_digits
+    double_digits.map do |subarray|
+      [subarray.first, subarray.last].join.to_i
+    end
+  end
+end
+
+p Puzzle.from_file('file').call
