b8a77c5cf89da9f4b48da73b634e611302d23031
Kaelon Swanton <kaelonswanton@gmail.com>
2024-02-04 18:06:30 -0800

    2015 1

--- 2015/1.rb
+++ 2015/1.rb
@@ -1,53 +1,54 @@
 # frozen_string_literal: true
 
+module PuzzleOne
 file = "(((())))))))"
+  class PartOne
+    attr_reader :file
 
-class PartOne
-  attr_reader :file
+    def initialize(file)
+      @file = file
+    end
 
-  def initialize(file)
-    @file = file
-  end
+    def call
+      scan_left - scan_right
+    end
 
-  def call
-    scan_left - scan_right
-  end
+    private
 
-  private
+    def scan_left
+      file.scan("(").length
+    end
 
-  def scan_left
-    file.scan("(").length
-  end
-
-  def scan_right
-    file.scan(")").length
-  end
-end
-puts PartOne.new(file).call
-
-class PartTwo
-  attr_reader :file
-
-  def initialize(file)
-    @file = file
-  end
-
-  def call
-    process_chars
-  end
-
-  private
-
-  def process_chars
-    counter = 0
-    file.chars.each_with_index do |char, index|
-      counter = count_chars(char, counter)
-      return (index + 1).to_s if counter == -1
+    def scan_right
+      file.scan(")").length
     end
   end
+  puts PartOne.new(file).call
 
-  def count_chars(char, counter)
-    char == "(" ? counter + 1 : counter - 1
+  class PartTwo
+    attr_reader :file
+
+    def initialize(file)
+      @file = file
+    end
+
+    def call
+      process_chars
+    end
+
+    private
+
+    def process_chars
+      counter = 0
+      file.chars.each_with_index do |char, index|
+        counter = count_chars(char, counter)
+        return (index + 1).to_s if counter == -1
+      end
+    end
+
+    def count_chars(char, counter)
+      char == "(" ? counter + 1 : counter - 1
+    end
   end
+  puts PartTwo.new(file).call
 end
-puts PartTwo.new(file).call
--- spec/2015/1_spec.rb
+++ spec/2015/1_spec.rb
@@ -3,14 +3,16 @@
 require_relative "../../2015/1"
 
 file = ")())())"
-RSpec.describe PartOne do
-  it "adds '('' and subtracts ')'" do
-    expect(PartOne.new(file).call).to eq(-3)
+RSpec.describe PuzzleOne do
+  describe PuzzleOne::PartOne do
+    it "adds '('' and subtracts ')'" do
+      expect(described_class.new(file).call).to eq(-3)
+    end
   end
-end
 
-RSpec.describe PartTwo do
-  it "finds index position+1 of -1" do
-    expect(PartTwo.new(file).call).to eq("1")
+  describe PuzzleOne::PartTwo do
+    it "finds index position+1 of -1" do
+      expect(described_class.new(file).call).to eq("1")
+    end
   end
 end
